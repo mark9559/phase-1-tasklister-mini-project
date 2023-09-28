85fcee1c5952e68b24d7f5cb6c0cabddb3367d6e
Mark Mwangi <mwangimark57@gmail.com>
2023-09-28 18:56:08 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const form = document.getElementById("create-task-form");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const taskDescription = event.target.querySelector("#new-task-description").value;
+      console.log("Task Description:", taskDescription);
+
+    });
+
 });
